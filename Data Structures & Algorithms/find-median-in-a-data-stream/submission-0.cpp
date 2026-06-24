56d7f86715eae5ef0043cceb31df95425430fa99
Eklavya <156961134+eklavya172004@users.noreply.github.com>
2026-06-25 04:18:14 +0530

    Add: find-median-in-a-data-stream - submission-0

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-0.cpp
@@ -0,0 +1,35 @@
+class MedianFinder {
+public:
+    priority_queue<int> maxHeap; // smaller half
+
+    priority_queue<int, vector<int>, greater<int>> minHeap; // larger half
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        if(maxHeap.empty() || num <= maxHeap.top()){
+            maxHeap.push(num);
+        }else{
+            minHeap.push(num);
+        }
+
+        if(maxHeap.size() > minHeap.size() + 1){
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        }
+
+        if(minHeap.size() > maxHeap.size()){
+                maxHeap.push(minHeap.top());
+                minHeap.pop();
+        }
+    }
+    
+    double findMedian() {
+    if (maxHeap.size() == minHeap.size()) {
+        return (maxHeap.top() + minHeap.top()) / 2.0;
+    }
+
+    return maxHeap.top();
+    }
+};
